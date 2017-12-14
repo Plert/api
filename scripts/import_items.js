ca1f2c47d578312d8ff8b4dcc8230f2e11477e48
Eduardo Salazar <esalazar922@gmail.com>
2017-12-14 09:54:37 +0800

    Removed Database

--- scripts/import_items.js
+++ scripts/import_items.js
@@ -3,13 +3,13 @@
 var Datastore = require('nedb');
 
 // 2. Confuration
-var itemDb = new Datastore({ filename: '../db/ItemInfo.db', autoload: true });
+var itemDb = new Datastore({ filename: 'db/ItemInfo.db', autoload: true });
 
 // 3. Index
 itemDb.ensureIndex({ fieldName: 'id', unique: true });
 
 // 4. Read from CSV
-const csvFilePath="seed/ItemInfo-104065422.csv"
+const csvFilePath="scripts/seed/ItemInfo-104065422.csv"
 const csv=require('csvtojson')
 csv().fromFile(csvFilePath).on('json',(jsonObj)=>{
   insertItem(jsonObj)
